1c46ae9160e8f37c2d805fab88270ed1c29d88f8
JoanaTsvetkova <yoana200409@gmail.com>
2023-12-16 23:38:12 +0200

    add firebase and react app

---
+++ app/src/components/Home.js
@@ -0,0 +1,11 @@
+export const Home = () => {
+    return(
+        <main id="home">
+        <div className="content">
+            <h1>FIND YOUR  <br/> <span>NEXT GAME</span></h1>
+            <a href="#" className="buy">BROWSE</a>
+        </div>
+
+    </main>
+    );
+}
